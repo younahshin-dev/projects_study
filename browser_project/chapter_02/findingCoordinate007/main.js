a6d63e6e21655d6d3599b958ad3b84d8a93bf37f
younahshin-dev <50191933+younahshin-dev@users.noreply.github.com>
2021-01-08 14:21:21 +0900

    Update main.js

--- browser_project/chapter_02/findingCoordinate007/main.js
+++ browser_project/chapter_02/findingCoordinate007/main.js
@@ -1,4 +1,6 @@
-const coordinateValue = document.querySelector(".coordinateValue");
+"use strict";
+const coordinateValue = document.querySelector(".coordinateValue>span");
+const coordinateImg = document.querySelector(".coordinateValue>img");
 const verticalLine = document.querySelector(".verticalLine");
 const horizenLine = document.querySelector(".horizenLine");
 
@@ -8,7 +10,12 @@ document.addEventListener("mousemove", (event) => {
   coordinateValue.innerText = `x : ${eventX}, y : ${eventY}`;
   coordinateValue.style.top = eventY + 5 + "px";
   coordinateValue.style.left = eventX + 5 + "px";
+  console.log(coordinateImg.height);
+  console.log(coordinateImg.style.height / 2);
 
-  verticalLine.style.left = eventX + "px";
+  coordinateImg.style.top = eventY - coordinateImg.height / 2 + "px";
+  coordinateImg.style.left = eventX - coordinateImg.width / 2 + "px";
+
   horizenLine.style.top = eventY + "px";
+  verticalLine.style.left = eventX + "px";
 });
