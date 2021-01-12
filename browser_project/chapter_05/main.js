1a203bb26748eeb2c34f27b1970919792cee6d28
younahshin-dev <50191933+younahshin-dev@users.noreply.github.com>
2021-01-12 13:03:34 +0900

    Add files via upload

---
+++ browser_project/chapter_05/main.js
@@ -0,0 +1,58 @@
+function checkValidation() {
+  const textInput = document.querySelector(".inputItem");
+  const itemValue = textInput.value;
+
+  if ("" === itemValue) {
+    return false;
+  }
+
+  return true;
+}
+
+function addItem() {
+  if (!checkValidation()) {
+    alert("Please enter your Shopping Item");
+    return;
+  }
+
+  const itemList = document.querySelector(".shoppingItems");
+  const textInput = document.querySelector(".inputItem");
+  const itemValue = textInput.value;
+  const itemElement = document.createElement("li");
+
+  itemElement.className = "item";
+  itemElement.innerHTML = `<span>${itemValue}</span><i class="fas fa-trash-alt"></i>`;
+  itemList.appendChild(itemElement);
+
+  bindTrashEvent();
+
+  textInput.value = "";
+}
+
+function bindTrashEvent() {
+  const trashIcons = document.querySelectorAll(".fa-trash-alt");
+  for (trashIcon of trashIcons) {
+    trashIcon.addEventListener("click", (event) => {
+      const currentTarget = event.currentTarget;
+      const removeItem = currentTarget.closest("li");
+      removeItem.remove();
+    });
+  }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  const addBtn = document.querySelector(".addBtn");
+  addBtn.addEventListener("click", () => {
+    addItem();
+  });
+
+  const textInput = document.querySelector(".inputItem");
+  textInput.addEventListener("keydown", (event) => {
+    if (13 !== event.keyCode) {
+      return;
+    }
+    addItem();
+  });
+
+  bindTrashEvent();
+});
